a24f37a8fbf73b5b76110cb992254ddb1acf5e0a
spikers-dev <spikers@inbox.com>
2022-06-13 20:36:14 +0300

    update 6

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -5,9 +5,10 @@ from brain_games import cli
 
 def main():
     print('Welcome to the Brain Games!')
+    # Приветствие
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
     main()
-    # Приветствие
-    cli.welcome_user()
+    
